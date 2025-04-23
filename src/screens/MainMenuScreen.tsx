96a744323fb328680eea69fb451717bcf95b7096
Jana Dahlmanns <janadahlmanns@gmail.com>
2025-04-23 23:40:01 +0200

    Fix tiny build error due to unsed declaration

--- src/screens/MainMenuScreen.tsx
+++ src/screens/MainMenuScreen.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useScreenSize } from '../hooks/useScreenSize';
 import { topics, lessonIds } from '../data/topics';
 import lessonStates from '../data/lessonStates';
