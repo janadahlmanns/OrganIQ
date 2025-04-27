6a2f231b91ada78819eb55002eea07913cc0f67b
janadahlmanns <112201997+janadahlmanns@users.noreply.github.com>
2025-04-27 07:19:02 +0200

    Feature ordering exercise (#7)
    * Implement ordering exercises
    
    * Add styling to ordering exercise
    
    * Fix colors on feedback
    
    * Fix domain redirect

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/OrganIQ/', // ← 👈 This line is the key
+  base: './', // ← 👈 This line is the key
 })
